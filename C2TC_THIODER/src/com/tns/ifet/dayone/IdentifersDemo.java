4091e9e969f2e230d4a6b8536623afe5c404bf47
Thioder eugine L <thioder5543@gmail.com>
2024-11-08 12:31:09 +0530

    dayone

---
+++ C2TC_THIODER/src/com/tns/ifet/dayone/IdentifersDemo.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.dayone;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number = 10;
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Thioder";
+		System.out.println("value is : "+ studentName);
+		}
+	}
